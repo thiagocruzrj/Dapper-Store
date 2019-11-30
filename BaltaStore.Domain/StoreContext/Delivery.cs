90d46a8a162c605e3225e6ee69f69f56769f89d5
Thiago Cruz <[thagocruz@gmail.com]>
2019-11-30 00:40:33 -0300

    Defining entities

---
+++ BaltaStore.Domain/StoreContext/Delivery.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BaltaStore.Domain.StoreContext
+{
+    public class Delivery
+    {
+        public DateTime CreateDate { get; set; }
+        public DateTime EstimatedDeliveryDate { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ BaltaStore.Domain/StoreContext/OrderItem.cs
@@ -0,0 +1,9 @@
+namespace BaltaStore.Domain.StoreContext
+{
+    public class OrderItem
+    {
+        public Product Product { get; set; }
+        public string Quantity { get; set; }
+        public string Price { get; set; }
+    }
+}
