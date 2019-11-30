481b9c2f2d51610f3c2d66fa3bd5090351b292ec
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2019-11-30 02:46:27 -0300

    Add collections

--- DapperStore.Domain/StoreContext/Entities/Customer.cs
+++ DapperStore.Domain/StoreContext/Entities/Customer.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using DapperStore.Domain.StoreContext.Entities;
 using DapperStore.Domain.StoreContext.ValueObjects;
 
@@ -6,6 +7,7 @@ namespace DapperStore.Domain.Entities.StoreContext
 {
     public class Customer
     {
+        private readonly IList<Address> _addresses;
         public Customer(Name name,
                         Document document,
                         Email email,
@@ -15,13 +17,21 @@ namespace DapperStore.Domain.Entities.StoreContext
             Document = document;
             Email = email;
             Phone = phone;
-            Addresses = new List<Address>();
+            _addresses = new List<Address>();
         }
         public Name Name { get; set; }
         public Document Document { get; private set; }
         public Email Email { get; private set; }
         public string Phone { get; private set; }
-        public IReadOnlyCollection<Address> Addresses { get; private set; }
+        // bodiless function, when the fuction has only one line
+        public IReadOnlyCollection<Address> Addresses => _addresses.ToArray();
+
+        public void AddAddress(Address address)
+        {
+            // Address validating
+            // Add address (added in a new private list, new list address instance, after that added on address property list)
+            _addresses.Add(address);
+        }
 
         public override string ToString()
         {
--- DapperStore.Domain/StoreContext/Entities/Order.cs
+++ DapperStore.Domain/StoreContext/Entities/Order.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using DapperStore.Domain.StoreContext.Entities.Enums;
 
 namespace DapperStore.Domain.Entities.StoreContext
 {
     public class Order
     {
+        private readonly IList<OrderItem> _items;
+        private readonly IList<Delivery> _deliveries;
         public Order(Customer customer)
         {
             Customer = customer;
@@ -13,21 +16,29 @@ namespace DapperStore.Domain.Entities.StoreContext
             Number = Guid.NewGuid().ToString().Replace("-","").Substring(0,8).ToUpper();
             CreateDate = DateTime.Now;
             Status = EOrderStatus.Created;
-            Items = new List<OrderItem>();
-            Deliveries = new List<Delivery>();
+            _items = new List<OrderItem>();
+            _deliveries = new List<Delivery>();
         }
 
         public Customer Customer { get; private set; }
         public string Number { get; private set; }
         public DateTime CreateDate { get; private set; }
         public EOrderStatus Status { get; private set; }
-        public IReadOnlyCollection<OrderItem> Items { get; private set; }
-        public IReadOnlyCollection<Delivery> Deliveries { get; private set; }
+        public IReadOnlyCollection<OrderItem> Items => _items.ToArray();
+        public IReadOnlyCollection<Delivery> Deliveries => _deliveries.ToArray();
 
         public void AddItem(OrderItem order)
         {
             // Validating item
             // Add order
+            _items.Add(order);
+        }
+
+        public void Delivery(Delivery delivery)
+        {
+            // Validating delivery
+            // Add delivery
+            _deliveries.Add(delivery);
         }
 
         // To place an order
