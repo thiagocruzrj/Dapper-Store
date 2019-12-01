083e3ae0a6f974e32e3f098d52ac2343160212e5
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2019-12-01 13:18:34 -0300

    Appling FastFailValidations

--- DapperStore.Domain/StoreContext/Commands/CustomerCommands/Input/CreateCustomerCommand.cs
+++ DapperStore.Domain/StoreContext/Commands/CustomerCommands/Input/CreateCustomerCommand.cs
@@ -1,11 +1,32 @@
+using DapperStore.Shared.Commands;
+using FluentValidator;
+using FluentValidator.Validation;
+
 namespace DapperStore.Domain.StoreContext.Commands.CustomerCommands.Input
 {
-    public class CreateCustomerCommand
+    public class CreateCustomerCommand : Notifiable, ICommand
     {
+        // Fail fast validation
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Document { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
+
+        public bool Valid()
+        {
+            AddNotifications(new ValidationContract()
+                .HasMinLen(FirstName, 3, "FirstName", "Name must be have 3 characters at minimum.")
+                .HasMaxLen(FirstName, 40, "FirstName", "Name must be have 40 characters at maximum.")
+                .HasMaxLen(LastName, 3, "LastName", "Surname must be have 3 characters at minimum.")
+                .HasMinLen(LastName, 40, "LastName", "Surname must be have 40 characters at minimum.")
+                .IsEmail(Email, "Email","Email is invalid")
+                .HasLen(Document, 11, "Document", "Invalid CPF"));
+            
+            return Valid();
+        }
+
+        // User exist on DB (Email)
+        // User exist on DB (CPF)
     }
 }
--- DapperStore.Domain/StoreContext/Commands/OrderCommands/Input/PlaceOrderCommand.cs
+++ DapperStore.Domain/StoreContext/Commands/OrderCommands/Input/PlaceOrderCommand.cs
@@ -1,12 +1,29 @@
 using System;
 using System.Collections.Generic;
+using DapperStore.Shared.Commands;
+using FluentValidator;
+using FluentValidator.Validation;
 
 namespace DapperStore.Domain.StoreContext.Commands.OrderCommands.Input
 {
-    public class PlaceOrderCommand
+    public class PlaceOrderCommand : Notifiable, ICommand
     {
+        public PlaceOrderCommand()
+        {
+            OrderItems = new List<OrderItemCommand>();
+        }
+
         public Guid Customer { get; set; }
         public IList<OrderItemCommand> OrderItems { get; set; }
+
+        public bool Valid()
+        {
+            AddNotifications(new ValidationContract()
+                .HasLen(Customer.ToString(), 36, "Customer", "Client identificador invalid")
+                .IsGreaterThan(OrderItems.Count, 0, "Items", "None of items order found")
+            );
+            return Valid();
+        }
     }
 
     public class OrderItemCommand
