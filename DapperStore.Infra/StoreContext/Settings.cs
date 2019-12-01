4950f08695d40a061eef3f4268fac89b4e6e081a
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2019-12-01 17:51:23 -0300

    Creating Data connection with Oracle DB

---
+++ DapperStore.Infra/StoreContext/Settings.cs
@@ -0,0 +1,7 @@
+namespace DapperStore.Infra.StoreContext
+{
+    public static class Settings
+    {
+        public static string ConnectionString = @"DATA SOURCE=AX;PASSWORD=CM;PERSIST SECURITY INFO=True;USER ID=AB;";
+    }
+}
