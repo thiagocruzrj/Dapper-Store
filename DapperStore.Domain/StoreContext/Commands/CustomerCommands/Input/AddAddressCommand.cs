b842fc0d0a520568c0c3e16864446655570e7b93
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2019-12-01 14:44:24 -0300

    Refactoring AddAddress errors

--- DapperStore.Domain/StoreContext/Commands/CustomerCommands/Input/AddAddressCommand.cs
+++ DapperStore.Domain/StoreContext/Commands/CustomerCommands/Input/AddAddressCommand.cs
@@ -1,9 +1,11 @@
 using System;
 using DapperStore.Domain.StoreContext.Entities.Enums;
+using DapperStore.Shared.Commands;
+using FluentValidator;
 
 namespace DapperStore.Domain.StoreContext.Commands.CustomerCommands.Input
 {
-    public class AddAddressCommand
+    public class AddAddressCommand : Notifiable, ICommand
     {
         public Guid Id { get; set; }
         public string Street { get; set; }
@@ -15,5 +17,10 @@ namespace DapperStore.Domain.StoreContext.Commands.CustomerCommands.Input
         public string Country { get; set; }
         public string ZipCode { get; set; }
         public EAddressType Type { get; set; }
+
+        public bool Valid()
+        {
+            return IsValid;
+        }
     }
 }
